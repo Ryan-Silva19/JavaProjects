5a856278271c6a00ed69672fc1fc0ae1e6987485
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-03-19 18:06:26 -0300

    adicionando os exercícios e organizando por pasta

---
+++ src/Aula6/CountFrom1To10.java
@@ -0,0 +1,17 @@
+package Aula6;
+
+
+public class CountFrom1To10 {
+
+	public static void main(String[] args) {
+
+		// Variáveis
+		int i = 1;
+		// Processamento / Saída
+		while (i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
