864ffb1c3d88d26ffd67304822d1277b88a39dd6
Ryan-Silva19 <ryanferreiradasilva2006@gmail.com>
2025-05-22 09:54:19 -0300

    Inicio do validador de senhas

---
+++ aula10/ValidadorDeSenhasIncomum.java
@@ -0,0 +1,18 @@
+package aula10;
+
+public class ValidadorDeSenhasIncomum {
+
+	public static void main(String[] args) {
+		// Variáveis
+		// Entrada
+		System.out.println("Bem vindo caro usuário, nesse programa vamos fazer um"
+				+ "validador de senha incomum, para verificar se sua senha está"
+				+ "cumprindo os requisitos, a senha deve conter: uma letra maiúscula"
+				+ ", um número primo, um caractere especial e não pode conter"
+				+ " vogais duplicadas seguidas.");
+		// Processamento
+		// Saída
+
+	}
+
+}
