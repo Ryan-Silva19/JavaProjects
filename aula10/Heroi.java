61b4b8d9f3a92ce5d7dfb4f22e613f36c68776ad
Ryan-Silva19 <ryanferreiradasilva2006@gmail.com>
2025-05-26 22:11:26 -0300

    Adicionando os métodos da classe RankingDeHeróis

---
+++ aula10/Heroi.java
@@ -0,0 +1,44 @@
+package aula10;
+
+public class Heroi {
+
+	private String nome;
+	private int forca;
+	private int inteligencia;
+	private int velocidade;
+	
+	public Heroi(String nome, int forca, int inteligencia, int velocidade){
+		this.nome = nome;
+		this.forca = forca;
+		this.inteligencia= inteligencia;
+		this.velocidade = velocidade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getForca() {
+		return forca;
+	}
+	
+	public int getInteligencia() {
+		return inteligencia;
+	}
+	
+	public int getVelocidade() {
+		return velocidade;
+	}
+	public void setForca(int newForca) {
+		this.forca = newForca;
+	}
+	public void setInteligencia(int newInteligencia) {
+		this.inteligencia = newInteligencia;
+	}
+	public void setVelocidade(int newVelocidade) {
+		this.velocidade = newVelocidade;
+	}
+	public int calcularPoderTotal(){
+		return forca + inteligencia + velocidade;
+	}
+}
