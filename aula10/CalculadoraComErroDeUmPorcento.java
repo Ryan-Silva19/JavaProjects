5914525888f19e0510ebcdf73135f3a50d4ae77c
Ryan-Silva19 <ryanferreiradasilva2006@gmail.com>
2025-05-31 22:00:12 -0300

    exercício da calculadora com erro de até 1%

---
+++ aula10/CalculadoraComErroDeUmPorcento.java
@@ -0,0 +1,39 @@
+package aula10;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class CalculadoraComErroDeUmPorcento {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random rd = new Random();
+		// Variáveis
+		double num1, num2, resultado, erro, erroPorcentagem;
+		String entrada;
+		char operacao;
+		// Entrada
+		System.out.print("Olá caro usuário, nesse programa vamos criar "
+				+ "uma pequena calculadora com com 4 operações matemáticas. Só que essa "
+				+ "calculadora ela tem um 'erro'\nde 1% que é calculado aleatoriamente ao "
+				+ "exibir o resultado, pense nisso como um arredondamento seguro só que pra cima\n"
+				+ "Insira o primeiro número: ");
+		num1 = sc.nextDouble();
+		System.out.print("Insira o segundo número: ");
+		num2 = sc.nextDouble();
+		sc.nextLine();
+		// Processamento
+		System.out.print("Insira a operçāo que deseja fazer(+, -, x, /): ");
+		entrada = sc.next();
+		if (!entrada.isEmpty()) {
+			operacao = entrada.charAt(0);
+		} else {
+			System.out.println("entrada inválida!");
+			sc.close();
+			return;
+		}
+		resultado = 0;
+
+	}
+
+}
